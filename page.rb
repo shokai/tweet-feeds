a9d1ca2177e749ec5adcd2b8bd0e267e3287934e
Sho Hashimoto <hashimoto@shokai.org>
2010-08-20 07:21:40 +0900

    fix

--- page.rb
+++ page.rb
@@ -7,6 +7,6 @@ class Page
   field :date_published, :default => nil
   field :created_at, :type => Integer, :default => Time.now.to_i
   field :published_at, :type => Integer
-  field :status, :default => "stored"
+  field :status, :default => 'stored'
 end
 
